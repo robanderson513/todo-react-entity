fafd96ce8146bf913d6d1c2177e59e7d0dde82a5
Rob <rob.anderson513@gmail.com>
2023-04-05 16:19:55 -0400

    Added cors policy

--- web-api/web-api/Program.cs
+++ web-api/web-api/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.EntityFrameworkCore;
 using WebApi.DAL;
 using WebApi.Services;
 
+var allowedOrigins = "_allowedOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,6 +12,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+    options.AddPolicy(allowedOrigins, policy => policy.WithOrigins("http://localhost:3000"))
+);
 
 builder.Services.AddScoped<UserService>();
 
@@ -26,6 +32,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(allowedOrigins);
 
 app.UseAuthorization();
 
